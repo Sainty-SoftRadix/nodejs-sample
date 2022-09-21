f9dddf95955d132655bd78c4da9c13c021d7f504
Sainty-SoftRadix <sainty@softradix.in>
2022-09-21 12:27:40 +0530

    Update server.js
    Comment added in server file

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const io = new Server(server);
 app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 });
-
+//Confirms the users connected
 io.on('connection', (socket) => {
     console.log("a user connected");
     socket.on("disconnect", () => {
